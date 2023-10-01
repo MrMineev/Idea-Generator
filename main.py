02e362ec0d2578d6e05162226b708ccfcf642c9d
Dragon267 <112898086+Dragon267@users.noreply.github.com>
2023-10-01 23:46:58 +0200

    prompt changes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_response(prompt):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        temperature=0.3,
+        temperature=0.8,
         max_tokens=500,
         top_p=1,
         frequency_penalty=0,
